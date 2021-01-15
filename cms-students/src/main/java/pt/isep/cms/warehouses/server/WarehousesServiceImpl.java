bb3b64c5c6505ee2d4b5792278d9540a4ed32eff
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-15 18:58:30 +0000

    Warehouses get method implemented. Misses add warehouse, delete and update.

--- cms-students/src/main/java/pt/isep/cms/warehouses/server/WarehousesServiceImpl.java
+++ cms-students/src/main/java/pt/isep/cms/warehouses/server/WarehousesServiceImpl.java
@@ -8,38 +8,47 @@ import java.util.Iterator;
 import pt.isep.cms.warehouses.client.WarehousesService;
 import pt.isep.cms.warehouses.shared.Warehouse;
 import pt.isep.cms.warehouses.shared.WarehouseDetails;
+import pt.isep.cms.DBConnection.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 @SuppressWarnings("serial")
 public class WarehousesServiceImpl extends RemoteServiceServlet implements
     WarehousesService {
 
-  private static final String[] warehousesNameData = new String[] {
-      "Warehouse X", "Warehouse Y", "Warehouse Z"};
-  
-  private final String[] warehousesTotalCapData = new String[] {
-      "5000", "2000", "3000"};
-  
- /* private final String[] warehousesLocationData = new String[] {
-      "Porto", "Lisboa", "Braganca"};*/
-      
-  private final HashMap<String, Warehouse> warehouses = new HashMap<String, Warehouse>();
+  private Connection connection = new DBConnection().getConnection();
+  private HashMap<String, Warehouse> warehouses = new HashMap<>();
 
   public WarehousesServiceImpl() {
-    initWarehouses();
+    getWarehouses();
   }
   
-  private void initWarehouses() {
-    // TODO: Create a real UID for each warehouse
-    //
-    for (int i = 0; i < warehousesNameData.length && i < warehousesTotalCapData.length/* && i < warehousesLocationData.length*/; ++i) {
-      Warehouse warehouse = new Warehouse(String.valueOf(i), warehousesNameData[i], warehousesTotalCapData[i]/*, warehousesLocationData[i]*/);
-      warehouses.put(warehouse.getId(), warehouse); 
+  private void getWarehouses() {
+    try {
+      PreparedStatement ps = connection.prepareStatement(
+              "select *  from WAREHOUSE"
+      );
+      ResultSet rSet = ps.executeQuery();
+      while (rSet.next()) {
+        Integer id = rSet.getInt("id");
+        String name = rSet.getString("name");
+        Integer totalCap = rSet.getInt("totalCapacity");
+        warehouses.put(id.toString(), new Warehouse(id.toString(), name, totalCap.toString()));
+      }
+    } catch (SQLException sqle) {
+      System.out.println("Database error while getting warehouses");
+      sqle.printStackTrace();
     }
   }
   
   public Warehouse addWarehouse(Warehouse warehouse) {
-    warehouse.setId(String.valueOf(warehouses.size()));
-    warehouses.put(warehouse.getId(), warehouse); 
+
     return warehouse;
   }
 
@@ -66,17 +75,48 @@ public class WarehousesServiceImpl extends RemoteServiceServlet implements
   
   public ArrayList<WarehouseDetails> getWarehouseDetails() {
     ArrayList<WarehouseDetails> warehouseDetails = new ArrayList<WarehouseDetails>();
-    
-    Iterator<String> it = warehouses.keySet().iterator();
-    while(it.hasNext()) { 
-      Warehouse warehouse = warehouses.get(it.next());          
-      warehouseDetails.add(warehouse.getLightWeightWarehouse());
+    try {
+      PreparedStatement ps = connection.prepareStatement(
+              "select *  from WAREHOUSE"
+      );
+      ResultSet rs = ps.executeQuery();
+
+      while (rs.next()) {
+        Integer id = rs.getInt("id");
+        String name = rs.getString("name");
+        String totalCap = rs.getString("totalCapacity");
+
+        warehouseDetails.add(
+                new Warehouse(id.toString(), name, totalCap.toString()).getLightWeightWarehouse()
+        );
+      }
+    } catch (SQLException sqle) {
+      System.out.println("Database error while getting Warehouses");
+      sqle.printStackTrace();
     }
-    
     return warehouseDetails;
   }
 
   public Warehouse getWarehouse(String id) {
+
+    try {
+      PreparedStatement ps = connection.prepareStatement(
+              "select *  from WAREHOUSE where id=?"
+      );
+      ps.setString(1, id);
+      ResultSet rSet = ps.executeQuery();
+      while (rSet.next()) {
+        Integer id1 = rSet.getInt("id");
+        String name = rSet.getString("name");
+        String totalCap = rSet.getString("totalCapacity");
+
+        warehouses.put(id1.toString(), new Warehouse(id1.toString(), name, totalCap.toString()));
+      }
+    } catch (SQLException sqle) {
+      System.out.println("Database error while retrieving teacher");
+      sqle.printStackTrace();
+    }
     return warehouses.get(id);
+
   }
 }
