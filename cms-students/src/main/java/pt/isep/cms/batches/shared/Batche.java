5fc30fcba607641758702a9d68cf5b4ef2ac3100
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-16 15:19:36 +0000

    Changes to models regarding Foreign Keys.

--- cms-students/src/main/java/pt/isep/cms/batches/shared/Batche.java
+++ cms-students/src/main/java/pt/isep/cms/batches/shared/Batche.java
@@ -1,21 +1,28 @@
 package pt.isep.cms.batches.shared;
 
+import pt.isep.cms.warehouses.shared.Warehouse;
+
+import javax.annotation.Nullable;
 import java.io.Serializable;
+import java.util.Date;
 
 @SuppressWarnings("serial")
 public class Batche implements Serializable {
     public String id;
     public String name;
     public String descrip;
-    public String manDate;
+    public Date manDate;
+    public @Nullable
+    Warehouse warehouse;
 	
 	public Batche() {}
 	
-	public Batche(String id, String name, String descrip, String manDate) {
+	public Batche(String id, String name, String descrip, Date manDate, Warehouse warehouse) {
 		this.id = id;
 		this.name = name;
 		this.descrip = descrip;
 		this.manDate = manDate;
+		this.warehouse = warehouse;
 	}
 	
 	public BatcheDetails getLightWeightBatche() {
@@ -28,7 +35,10 @@ public class Batche implements Serializable {
     public void setName(String name) { this.name = name; }
     public String getDescrip() { return descrip; }
     public void setDescrip(String descrip) { this.descrip = descrip; }
-    public String getManDate() { return manDate; }
-    public void setManDate(String manDate) { this.manDate = manDate; }
+    public Date getManDate() { return manDate; }
+    public void setManDate(Date manDate) { this.manDate = manDate; }
     public String getFullDetails() { return name + ": " + descrip; }
+    public Warehouse getWarehouse() { return warehouse; }
+    public void setWarehouse(Warehouse warehouse) { this.warehouse = warehouse; };
+
 }
--- cms-students/src/main/java/pt/isep/cms/shippingLocations/shared/ShippingLocation.java
+++ cms-students/src/main/java/pt/isep/cms/shippingLocations/shared/ShippingLocation.java
@@ -1,18 +1,24 @@
 package pt.isep.cms.shippingLocations.shared;
 
+import pt.isep.cms.warehouses.shared.Warehouse;
+
+import javax.annotation.Nullable;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class ShippingLocation implements Serializable {
     public String id;
     public String name;
+    // @Nullable Warehouse
+    // warehouse;
     
 	
 	public ShippingLocation() {}
 	
-	public ShippingLocation(String id, String name) {
+	public ShippingLocation(String id, String name /*Warehouse warehouse */) {
 		this.id = id;
 		this.name = name;
+		// this.warehouse = warehouse;
 		
 	}
 	
@@ -25,4 +31,6 @@ public class ShippingLocation implements Serializable {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
     public String getFullDetails() { return name; }
+  //  public Warehouse getWarehouse() { return warehouse; }
+   // public void setWarehouse(Warehouse warehouse) { this.warehouse = warehouse; }
 }
