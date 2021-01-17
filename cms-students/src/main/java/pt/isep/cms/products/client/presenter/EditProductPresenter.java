8c79ebb00b6bc2ae6735e1ac0ebda6935f573e77
Ana Rita Rodrigues <1191182@isep.ipp.pt>
2021-01-17 00:51:28 +0000

    update front-end about products

--- cms-students/src/main/java/pt/isep/cms/products/client/presenter/EditProductPresenter.java
+++ cms-students/src/main/java/pt/isep/cms/products/client/presenter/EditProductPresenter.java
@@ -8,10 +8,16 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.TextBox;
 import pt.isep.cms.products.client.ProductsServiceAsync;
 import pt.isep.cms.products.client.event.ProductUpdatedEvent;
 import pt.isep.cms.products.client.event.EditProductCancelledEvent;
 import pt.isep.cms.products.shared.Product;
+import pt.isep.cms.batches.client.BatchesService;
+import pt.isep.cms.batches.client.BatchesServiceAsync;
+import pt.isep.cms.batches.shared.Batche;
+
+import java.util.Locale;
 
 public class EditProductPresenter implements Presenter {
 	public interface Display {
@@ -24,6 +30,8 @@ public class EditProductPresenter implements Presenter {
 		HasValue<String> getDescrip();
 
 		HasValue<String> getPrice();
+		
+		HasValue<String> getBatch();
 
 		void show();
 
@@ -32,19 +40,23 @@ public class EditProductPresenter implements Presenter {
 
 	private Product product;
 	private final ProductsServiceAsync rpcService;
+	private final BatchesServiceAsync batService;
+	
 	private final HandlerManager eventBus;
 	private final Display display;
 
-	public EditProductPresenter(ProductsServiceAsync rpcService, HandlerManager eventBus, Display display) {
+	public EditProductPresenter(ProductsServiceAsync rpcService, BatchesServiceAsync batService, HandlerManager eventBus, Display display) {
 		this.rpcService = rpcService;
+		this.batService = batService;
 		this.eventBus = eventBus;
 		this.product = new Product();
 		this.display = display;
 		bind();
 	}
 
-	public EditProductPresenter(ProductsServiceAsync rpcService, HandlerManager eventBus, Display display, String id) {
+	public EditProductPresenter(ProductsServiceAsync rpcService, BatchesServiceAsync batService, HandlerManager eventBus, Display display, String id) {
 		this.rpcService = rpcService;
+		this.batService = batService;
 		this.eventBus = eventBus;
 		this.display = display;
 		bind();
@@ -55,6 +67,8 @@ public class EditProductPresenter implements Presenter {
 				EditProductPresenter.this.display.getName().setValue(product.getName());
 				EditProductPresenter.this.display.getDescrip().setValue(product.getDescrip());
 				EditProductPresenter.this.display.getPrice().setValue(product.getPrice());
+				EditProductPresenter.this.display.getBatch().setValue(product.getBatch().toString());
+
 			}
 
 			public void onFailure(Throwable caught) {
@@ -88,7 +102,17 @@ public class EditProductPresenter implements Presenter {
 		product.setName(display.getName().getValue());
 		product.setDescrip(display.getDescrip().getValue());
 		product.setPrice(display.getPrice().getValue());
+		batService.getBatch(display.getBatch().getValue(), new AsyncCallback<Batche>() {
 
+			public void onSuccess (Batche result) {
+				product.setBatch(result);
+			}
+			public void onFailure (Throwable caught) {
+				Window.alert("Error adding batche: provide a valid WAREHOUSE ID");
+			}
+		});
+			
+			
 		if (product.getId() == null) {
 			// Adding new product
 			rpcService.addProduct(product, new AsyncCallback<Product>() {
